6450490c38698135e5e2c8a1db5cbd871df40dd4
Shreya Sharma <codershreya33@gmail.com>
2026-08-01 15:38:38 +0530

    Update address and data constraints in transaction.sv

--- tb/transaction.sv
+++ tb/transaction.sv
@@ -8,10 +8,14 @@ class transaction;
   bit [1:0] bresp;
   bit [1:0] rresp;
   
-  constraint valid_addr_range{
-    awaddr inside {2,3,4,8,5,7};
-    araddr inside {11,15,13,16,12,14};
-                             }
-  constraint valid_data_range{wdata<10;rdata<10;}
+ constraint valid_addr_range {
+    awaddr inside {[0:127]};
+    araddr inside {[0:127]};
+}
+
+  constraint valid_data_range {
+    wdata inside {[32'h00000000 : 32'hFFFFFFFF]};
+}
+  
   
 endclass
