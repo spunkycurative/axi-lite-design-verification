8541adf58364bb7f09f88ef35f72c7fff7607b24
Shreya Sharma <codershreya33@gmail.com>
2026-08-01 17:41:31 +0530

    Refactor scoreboard class and fix formatting

--- tb/scoreboard.sv
+++ tb/scoreboard.sv
@@ -1,3 +1,5 @@
+
+
 class scoreboard;
   
   transaction tr,trd;
@@ -6,7 +8,17 @@ class scoreboard;
   
   bit [31:0] temp;
   bit [31:0] data[128]='{default:0};
+  /*temp = data[addr]; // temporary read from memory
+  data[5] = 32'hABCD1234;  // write to memory at index 5
+  temp = data[5];           // read from memory at index 5
+ ->data acts like your main memory or storage (like a small RAM).
+->temp acts as a temporary register to hold intermediate or read/write data.*/
   
+  function new(mailbox #(transaction)mbxms);
+    this.mbxms=mbxms;
+  endfunction
+  
+ 
   function new(mailbox #(transaction)mbxms);
     this.mbxms=mbxms;
   endfunction
@@ -35,14 +47,14 @@ class scoreboard;
             $display("[SCO] : DEC error");
           else if(tr.rresp==0 && tr.rdata==temp)//temp->data extracted from array ,rdata->data written by the mem
             $display("[SCO]: Data matched");
-          else
-                else if(tr.rresp==0 && tr.rdata==temp)//temp->data extracted from array ,rdata->data written by the mem
-            $display("[SCO]: Data matched");
-          else
+        else
             $display("[SCO]:Data mismatched");
         end
       $display("---------------------");
       ->sconext;
     end
+      
   endtask
+  
 endclass
+
